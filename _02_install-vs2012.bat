e4b1355a10a4465a07abadbf3ff3585a5846f996
Philip Proplesch <philip@proplesch.de>
2013-01-12 01:53:36 +0100

    Added unattended VS2012 installation script.

---
+++ _02_install-vs2012.bat
@@ -0,0 +1,1 @@
+powershell .\install-vs2012.ps1
---
+++ install-vs2012.ps1
@@ -0,0 +1,13 @@
+﻿# http://powershell.com/cs/blogs/tips/archive/2009/05/07/finding-cd-rom-drives.aspx
+function Get-IsoDrive {
+  Get-WmiObject win32_logicaldisk -Filter 'DriveType=5' | ForEach-Object { 
+    if($_.VolumeName.StartsWith('VS2012')) {
+      return $_.DeviceID
+    }
+  }
+}
+
+$installer = (Get-ChildItem (Get-IsoDrive) -Filter 'vs_*.exe').FullName
+$script =  [System.IO.Path]::Combine($pwd, 'unattended', 'VS2012_AdminDeployment.xml')
+
+Start-Process $installer -ArgumentList "/adminfile $script /quiet /norestart"
