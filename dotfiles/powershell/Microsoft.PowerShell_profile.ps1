e229488b444d8f80e00a6485eaad480cb294ec9a
Philip Proplesch <philip@proplesch.de>
2013-01-14 23:08:52 +0100

    Started with some new dotfiles.

---
+++ dotfiles/powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,9 @@
+﻿$dir = [System.IO.Path]::GetDirectoryName($PROFILE)
+
+Get-ChildItem $dir\scripts\*.ps1 | % {
+  . $_
+}
+
+# Load posh-git & posh-hg
+. "$dir\posh-git\profile.example.ps1"
+. "$dir\posh-hg\profile.example.ps1"
