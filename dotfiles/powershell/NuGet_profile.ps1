6bcd209a3f0dc6a050dca93bbbb377caf559877b
Philip Proplesch <philip@proplesch.de>
2013-01-15 22:25:22 +0100

    Added Configure-Environment function.

--- dotfiles/powershell/NuGet_profile.ps1
+++ dotfiles/powershell/NuGet_profile.ps1
@@ -1,3 +1,20 @@
+function Configure-Environment { 
+  # Enable line numbers in ALL languages.
+  $dte.Properties("TextEditor", "AllLanguages").Item("ShowLineNumbers").Value = $true
+
+  # Show empty environment on startup.
+  $dte.Properties("Environment", "Startup").Item("OnStartUp").Value = 4
+  
+  # Set indentations for web languages.
+  foreach($language in @("JavaScript", "CSS", "HTML")) {
+    Set-Indent 2 $language
+  }
+}
+
+function Get-FontSize {
+  Write-Host $dte.Properties("FontsAndColors", "TextEditor").Item("FontSize").Value
+}
+
 function Set-FontSize {
   param(
     [ValidateRange(6, 128)]
@@ -30,5 +47,23 @@ function Set-Theme {
   $vsSettingsLocation = [System.IO.Path]::Combine($vsLocation, "Settings")
   $vsSettingsFile = [System.IO.Path]::Combine($vsSettingsLocation, "$name.vssettings")
   
-  $dte.ExecuteCommand("Tools.ImportandExportSettings","/import:`"$vsSettingsFile`"")
+  if(Test-Path $vsSettingsFile) {
+    $dte.ExecuteCommand("Tools.ImportandExportSettings","/import:`"$vsSettingsFile`"")
+  }
+  else {
+    Write-Host "Could not find a theme with name '$name'."
+  }
+}
+
+function Set-Indent([int] $size, [string] $language) {
+
+  $dte.Properties("TextEditor", $language).Item("TabSize").Value = $size
+  $dte.Properties("TextEditor", $language).Item("IndentSize").Value = $size
+  
+  if($size -eq 4) {
+    $dte.Properties("TextEditor", $language).Item("InsertTabs").Value = $true
+  }
+  else {
+    $dte.Properties("TextEditor", $language).Item("InsertTabs").Value = $false
+  }
 }
