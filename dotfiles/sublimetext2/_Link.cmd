54437a686018cc4fb91842072bf5618e0a434373
Philip Proplesch <philip@proplesch.de>
2013-06-02 22:34:59 +0200

    Added Sublime Text 2 files

---
+++ dotfiles/sublimetext2/_Link.cmd
@@ -0,0 +1,3 @@
+mklink /D "%APPDATA%\Sublime Text 2\Installed Packages" "%CD%\Installed Packages"
+mklink /D "%APPDATA%\Sublime Text 2\Packages" "%CD%\Packages"
+mklink /D "%APPDATA%\Sublime Text 2\Pristine Packages" "%CD%\Pristine Packages"
