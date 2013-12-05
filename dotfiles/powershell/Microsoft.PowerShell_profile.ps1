e1bcf24d2166c028c2804012dbc829ae34e0b0b5
Philip Proplesch <philip@proplesch.de>
2013-12-05 10:59:01 +0100

    Updated .gitconfig.

--- dotfiles/powershell/Microsoft.PowerShell_profile.ps1
+++ dotfiles/powershell/Microsoft.PowerShell_profile.ps1
@@ -12,15 +12,20 @@ Import-Module "$dir\posh-git\posh-git"
 function prompt {
   $realLASTEXITCODE = $LASTEXITCODE
 
+  if(Get-IsAdmin -eq $true) {
+    $host.UI.RawUI.ForegroundColor = "Red"
+    Write-Host("[ADMIN] ") -NoNewline
+  }
+
   # Reset color, which can be messed up by Enable-GitColors
   $Host.UI.RawUI.ForegroundColor = $GitPromptSettings.DefaultForegroundColor
 
-  Write-Host($pwd) -nonewline
-  
+  Write-Host($pwd) -NoNewline  
   Write-VcsStatus
   
   $global:LASTEXITCODE = $realLASTEXITCODE
-  return "> "
+  
+  return "> "   
 }
 
 Enable-GitColors
--- dotfiles/powershell/scripts/posh-ui.ps1
+++ dotfiles/powershell/scripts/posh-ui.ps1
@@ -2,12 +2,12 @@ $host.UI.RawUI.ForegroundColor = "Green"
 $host.UI.RawUI.BackgroundColor = "Black"
 $host.UI.RawUI.CursorSize = 25
 
-$bufferSize = $host.ui.rawui.BufferSize
+$bufferSize = $host.UI.RawUI.BufferSize
 $bufferSize.Width = 140
 $bufferSize.Height = 3000
 $host.UI.RawUI.BufferSize = $bufferSize
 
-$windowSize = $host.ui.rawui.WindowSize
+$windowSize = $host.UI.RawUI.WindowSize
 $windowSize.Width = 140
 $windowSize.Height = 40
 $host.UI.RawUI.WindowSize = $windowSize
